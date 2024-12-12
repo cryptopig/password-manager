6d5d9c546efc3fcb9456af3f6dba7299954d2afa
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2024-12-12 09:02:10 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,22 @@
+// setup
+#include <bits/stdc++.h>
+using namespace std;
 
+const string letters = "abcdefghijklmnopqrstuvwxyz";
+const int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+const string characters = "~`!@#$%^&*()_-+={[}]|:<>?,./";
+
+// main
+int main() {
+    cout << "main";
+}
+
+// generate random password of specified length
+string generate_password(int length) {
+    srand(time(0));
+    string password = "t"*length;
+    for (int i = 0; i < length; i++) {
+        
+    }
+    return password;
+}
