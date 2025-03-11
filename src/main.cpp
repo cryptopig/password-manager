49f5b5250b8bdf1366d2276eca7ddaa5d3ea132c
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2025-03-11 22:17:53 +0000

    t

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 const string letters = "abcdefghijklmnopqrstuvwxyz";
-const int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+const string numbers = "1234567890";
 const string characters = "~`!@#$%^&*()_-+={[}]|:<>?,./";
 
 // generate random password of specified length
@@ -15,19 +15,42 @@ string generate_password(int length) {
     // index for password
     srand(time(0));
     string password(length, 'a');
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length/2; i++) {
         
         uniform_int_distribution<> distrib(0, length);
         int passIndex = distrib(gen);
-        password[passIndex] = 'b';
 
+        uniform_int_distribution<> distrib_1(0, letters.length());
+        int lettersIndex = distrib_1(gen);
+
+        password[passIndex] = letters[lettersIndex];
+
+    }
+
+    for (int i = 0; i < length / 3; i++) {
+        uniform_int_distribution<> distrib(0, length);
+        int passIndex = distrib(gen);
+
+        uniform_int_distribution<> distrib_1(0, characters.length());
+        int charactersIndex = distrib_1(gen);
+
+        password[passIndex] = characters[charactersIndex];
+    }
+
+    for (int i = 0; i < length / 3; i++) {
+        uniform_int_distribution<> distrib(0, length);
+        int passIndex = distrib(gen);
+
+        uniform_int_distribution<> distrib_1(0, numbers.length());
+        int numbersIndex = distrib_1(gen);
+
+        password[passIndex] = numbers[numbersIndex];
     }
     return password;
 }
 
 // main
 int main() {
-    cout << "main" << endl;
+    cout << "password: ";
     cout << generate_password(8);
-
 }
